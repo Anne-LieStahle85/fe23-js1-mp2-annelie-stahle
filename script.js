eaee3ded87feab0a58498ca315d66202ea09c592
Anne-LieStahle85 <149506235+Anne-LieStahle85@users.noreply.github.com>
2023-11-24 13:26:50 +0100

    fixade knappvalen

--- script.js
+++ script.js
@@ -17,49 +17,51 @@ sen ändras texten till vad dom valde*/
 const spela = ["sten", "sax", "påse"];
 let playButton = document.querySelector("#option");
 playButton.addEventListener("click", (event) => {
-  const computerChoice = Math.floor(Math.random() * spela.length);
+  if (event.target.tagName === "BUTTON") {
+    const computerChoice = Math.floor(Math.random() * spela.length);
 
-  document.querySelector(".playerText").innerText =
-    "Du valde " + spela[event.target.id];
-  document.querySelector(".computerText").innerText =
-    "Datorn valde " + spela[computerChoice];
+    document.querySelector(".playerText").innerText =
+      "Du valde " + spela[event.target.id];
+    document.querySelector(".computerText").innerText =
+      "Datorn valde " + spela[computerChoice];
 
-  /*Här jämför jag först om valen blir lika i if-satsen,sen i else if så
+    /*Här jämför jag först om valen blir lika i if-satsen,sen i else if så
   jämför jag när alternativen blir att spelaren vinner, och i else så blir
   alla andra fall att datorn vinner */
 
-  if (event.target.id == computerChoice) {
-    console.log(event.target.id, computerChoice);
-    console.log("LIKA");
-    document.querySelectorAll(".score")[0].innerText = "Det blev lika";
-    document.querySelectorAll(".score")[1].innerText = "Det blev lika";
-  } else if (
-    (event.target.id == 0 && computerChoice == 1) ||
-    (event.target.id == 1 && computerChoice == 2) ||
-    (event.target.id == 2 && computerChoice == 0)
-  ) {
-    console.log("spelaren vinner");
-    document.querySelectorAll(".score")[0].innerText = "Du vinner";
-    document.querySelectorAll(".score")[1].innerText = "Datorn förlorar";
-    playerScore++;
-    console.log(playerScore);
-    document.querySelector(".playerCount").innerText =
-      "Dina poäng: " + playerScore;
-    if (playerScore == 3) {
-      document.querySelector(".winner").innerText =
-        "Vinnaren är:\n" + playerName.textContent;
-      disableButtons();
-    }
-  } else {
-    console.log("datorn vinner");
-    document.querySelectorAll(".score")[0].innerText = "Du förlorar";
-    document.querySelectorAll(".score")[1].innerText = "Datorn vinner";
-    computerScore++;
-    document.querySelector(".computerCount").innerText =
-      "Datorns poäng: " + computerScore;
-    if (computerScore == 3) {
-      document.querySelector(".winner").innerText = "Vinnaren är:\n Datorn";
-      disableButtons();
+    if (event.target.id == computerChoice) {
+      console.log(event.target.id, computerChoice);
+      console.log("LIKA");
+      document.querySelectorAll(".score")[0].innerText = "Det blev lika";
+      document.querySelectorAll(".score")[1].innerText = "Det blev lika";
+    } else if (
+      (event.target.id == 0 && computerChoice == 1) ||
+      (event.target.id == 1 && computerChoice == 2) ||
+      (event.target.id == 2 && computerChoice == 0)
+    ) {
+      console.log("spelaren vinner");
+      document.querySelectorAll(".score")[0].innerText = "Du vinner";
+      document.querySelectorAll(".score")[1].innerText = "Datorn förlorar";
+      playerScore++;
+      console.log(playerScore);
+      document.querySelector(".playerCount").innerText =
+        "Dina poäng: " + playerScore;
+      if (playerScore == 3) {
+        document.querySelector(".winner").innerText =
+          "Vinnaren är:\n" + playerName.textContent;
+        disableButtons();
+      }
+    } else {
+      console.log("datorn vinner");
+      document.querySelectorAll(".score")[0].innerText = "Du förlorar";
+      document.querySelectorAll(".score")[1].innerText = "Datorn vinner";
+      computerScore++;
+      document.querySelector(".computerCount").innerText =
+        "Datorns poäng: " + computerScore;
+      if (computerScore == 3) {
+        document.querySelector(".winner").innerText = "Vinnaren är:\n Datorn";
+        disableButtons();
+      }
     }
   }
 });
